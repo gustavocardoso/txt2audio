fc7b2d92037360240fb981cd14bd272ecf84c866
Gustavo Cardoso <gustavocardoso@gmail.com>
2020-03-28 17:02:05 -0700

    Remove ogg responsabilities from the main file

--- src/index.js
+++ src/index.js
@@ -1,73 +1,62 @@
-const textToSpeech = require('@google-cloud/text-to-speech')
-const fs = require('fs')
-const util = require('util')
-const report = require('yurnalist')
-const ffmpeg = require('fluent-ffmpeg')
+const textToSpeech = require("@google-cloud/text-to-speech");
+const fs = require("fs");
+const util = require("util");
+const report = require("yurnalist");
 
-const client = new textToSpeech.TextToSpeechClient()
+const convertToOgg = require("./ogg");
 
-const convertAudioToOgg = async (fileName, path) => {
-  return new Promise(async (resolve, reject) => {
-    return ffmpeg()
-      .input(`${path}/${fileName}.mp3`)
-      .toFormat('ogg')
-      .output(`${path}/${fileName}.ogg`)
-      .on('end', resolve)
-      .on('error', reject)
-      .run()
-  })
-}
+const client = new textToSpeech.TextToSpeechClient();
 
 const text2Audio = ({
   text,
   filename,
   path,
   ogg = false,
-  gender = 'female',
+  gender = "female",
   debug = false
 }) => {
-  gender = gender.toUpperCase()
+  gender = gender.toUpperCase();
   const request = {
     input: { text },
-    voice: { languageCode: 'en-US-Standard-C', ssmlGender: gender },
-    audioConfig: { audioEncoding: 'MP3' }
-  }
+    voice: { languageCode: "en-US-Standard-C", ssmlGender: gender },
+    audioConfig: { audioEncoding: "MP3" }
+  };
 
   if (debug) {
-    report.warn('Debugging request object:')
-    report.inspect(request)
+    report.warn("Debugging request object:");
+    report.inspect(request);
   }
 
   return {
     generateAudio: async () => {
-      const spinner = report.activity()
+      const spinner = report.activity();
 
-      spinner.tick(`Fetching audio for: ${text}`)
+      spinner.tick(`Fetching audio for: ${text}`);
 
       try {
-        const [response] = await client.synthesizeSpeech(request)
-        const writeFile = util.promisify(fs.writeFile)
-        const fileNameMp3 = `${filename}.mp3`
+        const [response] = await client.synthesizeSpeech(request);
+        const writeFile = util.promisify(fs.writeFile);
+        const fileNameMp3 = `${filename}.mp3`;
 
         await writeFile(
           `${path}/${fileNameMp3}`,
           response.audioContent,
-          'binary'
-        )
+          "binary"
+        );
 
         if (ogg) {
-          spinner.tick(`Converting ${fileNameMp3} to ogg`)
-          await convertAudioToOgg(filename, path)
+          spinner.tick(`Converting ${fileNameMp3} to ogg`);
+          await convertToOgg(filename, path);
         }
 
-        report.success(`mp3 and ogg files for: ${text}`)
+        report.success(`mp3 and ogg files for: ${text}`);
       } catch (error) {
-        report.error(error)
+        report.error(error);
       }
 
-      spinner.end()
+      spinner.end();
     }
-  }
-}
+  };
+};
 
-module.exports = text2Audio
+module.exports = text2Audio;
---
+++ src/ogg.js
@@ -0,0 +1,15 @@
+const ffmpeg = require("fluent-ffmpeg");
+
+const convertToOgg = async (fileName, path) => {
+  return new Promise(async (resolve, reject) => {
+    return ffmpeg()
+      .input(`${path}/${fileName}.mp3`)
+      .toFormat("ogg")
+      .output(`${path}/${fileName}.ogg`)
+      .on("end", resolve)
+      .on("error", reject)
+      .run();
+  });
+};
+
+module.exports = convertToOgg;
